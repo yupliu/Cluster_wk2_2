8a43c1e265120373a504108f833779e0143d7dab
Alan Yu <yupliu@users.noreply.github.com>
2017-09-13 19:48:15 -0400

    Done

--- Cluster_wk2_2/Cluster_wk2_2/Cluster_wk2_2.py
+++ Cluster_wk2_2/Cluster_wk2_2/Cluster_wk2_2.py
@@ -117,9 +117,9 @@ def train_lsh(data, num_vector=16, seed=None):
     for data_index, bin_index in enumerate(bin_indices):
         if bin_index not in table:
             # If no list yet exists for this bin, assign the bin an empty list.
-            table[bin_index] = ... # YOUR CODE HERE
+            table[bin_index] = [] # YOUR CODE HERE
         # Fetch the list of document ids associated with the bin and add the document id to the end.
-        ... # YOUR CODE HERE
+        table[bin_index].append(data_index) # YOUR CODE HERE
 
     model = {'data': data,
              'bin_index_bits': bin_index_bits,
@@ -129,3 +129,343 @@ def train_lsh(data, num_vector=16, seed=None):
              'num_vector': num_vector}
     
     return model
+
+model = train_lsh(corpus, num_vector=16, seed=143)
+table = model['table']
+if   0 in table and table[0]   == [39583] and \
+   143 in table and table[143] == [19693, 28277, 29776, 30399]:
+    print 'Passed!'
+else:
+    print 'Check your code.'
+
+wiki[wiki['name'] == 'Barack Obama']
+
+wiki_row_number = wiki
+wiki_row_number = wiki_row_number.add_row_number()
+wiki_row_number[wiki_row_number['name']=='Barack Obama']
+obama_id = wiki_row_number[wiki_row_number['name']=='Barack Obama']['id']
+obama_id = obama_id[0]
+
+biden_id = wiki_row_number[wiki_row_number['name']=='Joe Biden']['id']
+biden_id = biden_id[0]
+
+obama_bin = []
+for i in table:
+    if obama_id in set(table[i]):
+        print i
+        obama_bin.append(i)
+
+biden_bin = []
+for i in table:
+    if biden_id in set(table[i]):
+        print i
+        biden_bin.append(i)
+
+#bin indices, row number is document id, value is the bin number
+bin_indices = model['bin_indices']
+bin_index_bits = model['bin_index_bits']
+
+biden_bits = bin_index_bits[biden_id]
+obama_bits = bin_index_bits[obama_id]
+diff_bit = biden_bits - obama_bits
+diff_bit_same = diff_bit[diff_bit==False]
+len(diff_bit_same)
+
+#get the encoded obama document
+obama_vector = corpus[obama_id].toarray()
+biden_vector = corpus[biden_id].toarray()
+diff = obama_vector - biden_vector
+diff_same = diff[diff==0]
+
+
+wiki[wiki['name']=='Wynn Normington Hugh-Jones']
+print np.array(model['bin_index_bits'][22745], dtype=int) # list of 0/1's
+print model['bin_indices'][22745] # integer format
+model['bin_index_bits'][35817] == model['bin_index_bits'][22745]
+
+model['table'][model['bin_indices'][35817]]
+
+doc_ids = list(model['table'][model['bin_indices'][35817]])
+doc_ids.remove(35817) # display documents other than Obama
+
+docs = wiki_row_number.filter_by(values=doc_ids, column_name='id') # filter by id column
+docs
+
+def cosine_distance(x, y):
+    xy = x.dot(y.T)
+    dist = xy/(norm(x)*norm(y))
+    return 1-dist[0,0]
+
+obama_tf_idf = corpus[35817,:]
+biden_tf_idf = corpus[24478,:]
+
+print '================= Cosine distance from Barack Obama'
+print 'Barack Obama - {0:24s}: {1:f}'.format('Joe Biden',
+                                             cosine_distance(obama_tf_idf, biden_tf_idf))
+for doc_id in doc_ids:
+    doc_tf_idf = corpus[doc_id,:]
+    print 'Barack Obama - {0:24s}: {1:f}'.format(wiki[doc_id]['name'],
+                                                 cosine_distance(obama_tf_idf, doc_tf_idf))
+from itertools import combinations
+num_vector = 16
+search_radius = 3
+for diff in combinations(range(num_vector), search_radius):
+    print diff
+
+
+def search_nearby_bins(query_bin_bits, table, search_radius=2, initial_candidates=set()):
+    """
+    For a given query vector and trained LSH model, return all candidate neighbors for
+    the query among all bins within the given search radius.
+    
+    Example usage
+    -------------
+    >>> model = train_lsh(corpus, num_vector=16, seed=143)
+    >>> q = model['bin_index_bits'][0]  # vector for the first document
+  
+    >>> candidates = search_nearby_bins(q, model['table'])
+    """
+    num_vector = len(query_bin_bits)
+    powers_of_two = 1 << np.arange(num_vector-1, -1, -1)
+    
+    # Allow the user to provide an initial set of candidates.
+    candidate_set = copy(initial_candidates)
+    
+    for different_bits in combinations(range(num_vector), search_radius):       
+        # Flip the bits (n_1,n_2,...,n_r) of the query bin to produce a new bit vector.
+        ## Hint: you can iterate over a tuple like a list
+        alternate_bits = copy(query_bin_bits)
+        for i in different_bits:
+            alternate_bits[i] = not alternate_bits[i] # YOUR CODE HERE 
+        
+        # Convert the new bit vector to an integer index
+        nearby_bin = alternate_bits.dot(powers_of_two)
+        
+        # Fetch the list of documents belonging to the bin indexed by the new bit vector.
+        # Then add those documents to candidate_set
+        # Make sure that the bin exists in the table!
+        # Hint: update() method for sets lets you add an entire list to the set
+        if nearby_bin in table:
+            candidate_set.update(table[nearby_bin]) # YOUR CODE HERE: Update candidate_set with the documents in this bin.
+            
+    return candidate_set
+
+
+obama_bin_index = model['bin_index_bits'][35817] # bin index of Barack Obama
+candidate_set = search_nearby_bins(obama_bin_index, model['table'], search_radius=0)
+if candidate_set == set([35817, 21426, 53937, 39426, 50261]):
+    print 'Passed test'
+else:
+    print 'Check your code'
+print 'List of documents in the same bin as Obama: 35817, 21426, 53937, 39426, 50261'
+
+candidate_set = search_nearby_bins(obama_bin_index, model['table'], search_radius=1, initial_candidates=candidate_set)
+if candidate_set == set([39426, 38155, 38412, 28444, 9757, 41631, 39207, 59050, 47773, 53937, 21426, 34547,
+                         23229, 55615, 39877, 27404, 33996, 21715, 50261, 21975, 33243, 58723, 35817, 45676,
+                         19699, 2804, 20347]):
+    print 'Passed test'
+else:
+    print 'Check your code'
+
+def query(vec, model, k, max_search_radius):
+  
+    data = model['data']
+    table = model['table']
+    random_vectors = model['random_vectors']
+    num_vector = random_vectors.shape[1]
+    
+    
+    # Compute bin index for the query vector, in bit representation.
+    bin_index_bits = (vec.dot(random_vectors) >= 0).flatten()
+    
+    # Search nearby bins and collect candidates
+    candidate_set = set()
+    for search_radius in xrange(max_search_radius+1):
+        candidate_set = search_nearby_bins(bin_index_bits, table, search_radius, initial_candidates=candidate_set)
+    
+    # Sort candidates by their true distances from the query
+    nearest_neighbors = graphlab.SFrame({'id':candidate_set})
+    candidates = data[np.array(list(candidate_set)),:]
+    nearest_neighbors['distance'] = pairwise_distances(candidates, vec, metric='cosine').flatten()
+    
+    return nearest_neighbors.topk('distance', k, reverse=True), len(candidate_set)
+
+query(corpus[35817,:], model, k=10, max_search_radius=3)
+
+query(corpus[35817,:], model, k=10, max_search_radius=3)[0].join(wiki_row_number[['id', 'name']], on='id').sort('distance')
+
+wiki[wiki['name']=='Barack Obama']
+
+wiki = wiki_row_number
+
+num_candidates_history = []
+query_time_history = []
+max_distance_from_query_history = []
+min_distance_from_query_history = []
+average_distance_from_query_history = []
+
+for max_search_radius in xrange(17):
+    start=time.time()
+    result, num_candidates = query(corpus[35817,:], model, k=10,
+                                   max_search_radius=max_search_radius)
+    end=time.time()
+    query_time = end-start
+    
+    print 'Radius:', max_search_radius
+    print result.join(wiki[['id', 'name']], on='id').sort('distance')
+    
+    average_distance_from_query = result['distance'][1:].mean()
+    max_distance_from_query = result['distance'][1:].max()
+    min_distance_from_query = result['distance'][1:].min()
+    
+    num_candidates_history.append(num_candidates)
+    query_time_history.append(query_time)
+    average_distance_from_query_history.append(average_distance_from_query)
+    max_distance_from_query_history.append(max_distance_from_query)
+    min_distance_from_query_history.append(min_distance_from_query)
+
+plt.figure(figsize=(7,4.5))
+plt.plot(num_candidates_history, linewidth=4)
+plt.xlabel('Search radius')
+plt.ylabel('# of documents searched')
+plt.rcParams.update({'font.size':16})
+plt.tight_layout()
+
+plt.figure(figsize=(7,4.5))
+plt.plot(query_time_history, linewidth=4)
+plt.xlabel('Search radius')
+plt.ylabel('Query time (seconds)')
+plt.rcParams.update({'font.size':16})
+plt.tight_layout()
+
+plt.figure(figsize=(7,4.5))
+plt.plot(average_distance_from_query_history, linewidth=4, label='Average of 10 neighbors')
+plt.plot(max_distance_from_query_history, linewidth=4, label='Farthest of 10 neighbors')
+plt.plot(min_distance_from_query_history, linewidth=4, label='Closest of 10 neighbors')
+plt.xlabel('Search radius')
+plt.ylabel('Cosine distance of neighbors')
+plt.legend(loc='best', prop={'size':15})
+plt.rcParams.update({'font.size':16})
+plt.tight_layout()
+plt.show()
+plt.close()
+
+def brute_force_query(vec, data, k):
+    num_data_points = data.shape[0]
+    
+    # Compute distances for ALL data points in training set
+    nearest_neighbors = graphlab.SFrame({'id':range(num_data_points)})
+    nearest_neighbors['distance'] = pairwise_distances(data, vec, metric='cosine').flatten()
+    
+    return nearest_neighbors.topk('distance', k, reverse=True)
+
+max_radius = 17
+precision = {i:[] for i in xrange(max_radius)}
+average_distance  = {i:[] for i in xrange(max_radius)}
+query_time  = {i:[] for i in xrange(max_radius)}
+
+np.random.seed(0)
+num_queries = 10
+for i, ix in enumerate(np.random.choice(corpus.shape[0], num_queries, replace=False)):
+    print('%s / %s' % (i, num_queries))
+    ground_truth = set(brute_force_query(corpus[ix,:], corpus, k=25)['id'])
+    # Get the set of 25 true nearest neighbors
+    
+    for r in xrange(1,max_radius):
+        start = time.time()
+        result, num_candidates = query(corpus[ix,:], model, k=10, max_search_radius=r)
+        end = time.time()
+
+        query_time[r].append(end-start)
+        # precision = (# of neighbors both in result and ground_truth)/10.0
+        precision[r].append(len(set(result['id']) & ground_truth)/10.0)
+        average_distance[r].append(result['distance'][1:].mean())
+
+plt.figure(figsize=(7,4.5))
+plt.plot(range(1,17), [np.mean(average_distance[i]) for i in xrange(1,17)], linewidth=4, label='Average over 10 neighbors')
+plt.xlabel('Search radius')
+plt.ylabel('Cosine distance')
+plt.legend(loc='best', prop={'size':15})
+plt.rcParams.update({'font.size':16})
+plt.tight_layout()
+
+plt.figure(figsize=(7,4.5))
+plt.plot(range(1,17), [np.mean(precision[i]) for i in xrange(1,17)], linewidth=4, label='Precison@10')
+plt.xlabel('Search radius')
+plt.ylabel('Precision')
+plt.legend(loc='best', prop={'size':15})
+plt.rcParams.update({'font.size':16})
+plt.tight_layout()
+
+plt.figure(figsize=(7,4.5))
+plt.plot(range(1,17), [np.mean(query_time[i]) for i in xrange(1,17)], linewidth=4, label='Query time')
+plt.xlabel('Search radius')
+plt.ylabel('Query time (seconds)')
+plt.legend(loc='best', prop={'size':15})
+plt.rcParams.update({'font.size':16})
+plt.tight_layout()
+plt.show()
+plt.close()
+
+precision = {i:[] for i in xrange(5,20)}
+average_distance  = {i:[] for i in xrange(5,20)}
+query_time = {i:[] for i in xrange(5,20)}
+num_candidates_history = {i:[] for i in xrange(5,20)}
+ground_truth = {}
+
+np.random.seed(0)
+num_queries = 10
+docs = np.random.choice(corpus.shape[0], num_queries, replace=False)
+
+for i, ix in enumerate(docs):
+    ground_truth[ix] = set(brute_force_query(corpus[ix,:], corpus, k=25)['id'])
+    # Get the set of 25 true nearest neighbors
+
+for num_vector in xrange(5,20):
+    print('num_vector = %s' % (num_vector))
+    model = train_lsh(corpus, num_vector, seed=143)
+    
+    for i, ix in enumerate(docs):
+        start = time.time()
+        result, num_candidates = query(corpus[ix,:], model, k=10, max_search_radius=3)
+        end = time.time()
+        
+        query_time[num_vector].append(end-start)
+        precision[num_vector].append(len(set(result['id']) & ground_truth[ix])/10.0)
+        average_distance[num_vector].append(result['distance'][1:].mean())
+        num_candidates_history[num_vector].append(num_candidates)
+
+plt.figure(figsize=(7,4.5))
+plt.plot(range(5,20), [np.mean(average_distance[i]) for i in xrange(5,20)], linewidth=4, label='Average over 10 neighbors')
+plt.xlabel('# of random vectors')
+plt.ylabel('Cosine distance')
+plt.legend(loc='best', prop={'size':15})
+plt.rcParams.update({'font.size':16})
+plt.tight_layout()
+
+plt.figure(figsize=(7,4.5))
+plt.plot(range(5,20), [np.mean(precision[i]) for i in xrange(5,20)], linewidth=4, label='Precison@10')
+plt.xlabel('# of random vectors')
+plt.ylabel('Precision')
+plt.legend(loc='best', prop={'size':15})
+plt.rcParams.update({'font.size':16})
+plt.tight_layout()
+
+plt.figure(figsize=(7,4.5))
+plt.plot(range(5,20), [np.mean(query_time[i]) for i in xrange(5,20)], linewidth=4, label='Query time (seconds)')
+plt.xlabel('# of random vectors')
+plt.ylabel('Query time (seconds)')
+plt.legend(loc='best', prop={'size':15})
+plt.rcParams.update({'font.size':16})
+plt.tight_layout()
+
+plt.figure(figsize=(7,4.5))
+plt.plot(range(5,20), [np.mean(num_candidates_history[i]) for i in xrange(5,20)], linewidth=4,
+         label='# of documents searched')
+plt.xlabel('# of random vectors')
+plt.ylabel('# of documents searched')
+plt.legend(loc='best', prop={'size':15})
+plt.rcParams.update({'font.size':16})
+plt.tight_layout()
+plt.show()
+plt.close()
